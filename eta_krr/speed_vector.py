e60654b00db9868cc064287bb2ab3935726eb389
Asif Rehan <asif.rehan9@gmail.com>
2015-05-28 04:19:29 -0400

    ADD: added speed_storage.p

--- eta_krr/speed_vector.py
+++ eta_krr/speed_vector.py
@@ -1,4 +1,4 @@
-'''ss
+'''
 Created on May 27, 2015
 
 @author: asr13006
@@ -6,6 +6,7 @@ Created on May 27, 2015
 import os
 import pandas as pd
 import numpy as np
+import pickle
 import dateutil.parser as parser
 #----------------------------------------------------------------------------- 
 src_fldr = os.path.join(r'C:\Users\asr13006\Google Drive\UConn MS',
@@ -15,14 +16,14 @@ node_files = [f for f in os.listdir(os.path.join(src_fldr,'node_files'))  \
 road_files = [f for f in os.listdir(os.path.join(src_fldr,'road_files'))  \
              if os.path.isfile(os.path.join(src_fldr,'road_files',f))]
 nd_rd_pair_files = zip(node_files, road_files)
-n_road = 176
+n_road = 177
 #----------------------------------------------------------------------------- 
 
 speed_storage = {}
 for i in xrange(n_road):
     speed_storage[i] = []
 
-for (v, e) in nd_rd_pair_files[0:1]:
+for (v, e) in nd_rd_pair_files:
     node_f = pd.read_csv(os.path.join(src_fldr, 'node_files', v),
                          index_col=0,
                          usecols = [0,3,4])
@@ -30,7 +31,7 @@ for (v, e) in nd_rd_pair_files[0:1]:
     road_f['speed'] = ""
     ts_idx = node_f[node_f.timestamp != '0'].index
     delta_ts_ticks = zip(ts_idx[:-1], ts_idx[1:])
-    for (t_prev, t_nxt) in delta_ts_ticks[:10]:
+    for (t_prev, t_nxt) in delta_ts_ticks:
         includd_0_spd = road_f[t_prev:t_nxt]
         excludd_0_spd = includd_0_spd[includd_0_spd.length != 0]
         if len(excludd_0_spd.index) != 0:
@@ -40,12 +41,9 @@ for (v, e) in nd_rd_pair_files[0:1]:
                               t_delta.total_seconds()) 
             for idx in excludd_0_spd.index:
                 road_f.speed[idx] = avg_speed
-        
-            
-#==============================================================================
-# to make timestamp into a 
-#==============================================================================
-for v in node_files[0:1]:
-    node_df = pd.read_csv(os.path.join(src_fldr, 'node_files', v))
-    for i in xrange(len(f)):
-        node_df.timestamp[i] = node_df.timestamp[i].replace(' ', 'T')
+    for idx in road_f[road_f.speed!=''].index:
+        speed_storage[road_f.road_id[idx]].append(road_f.speed[idx])
+pickle.dump(speed_storage, open(os.path.join(src_fldr, '..','speed_storage.p'),
+                                'rb'))
+for i in xrange(n_road):
+    np.mean(np.array(speed_storage[i]))
